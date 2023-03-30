954669735a2fda00c37f4fbab25eb8e94ca52dc8
Ryan Whitehouse <rwhitehouse.rw@gmail.com>
2023-03-30 15:19:15 +0200

    Updated to include onlyOwner

--- nft/erc721.sol
+++ nft/erc721.sol
@@ -12,13 +12,21 @@ contract ERC721 {
     constructor() {
     }
 
+    // Ensures that only the owner of the token can perform certain functions
+    modifier onlyOwner(address _owner) {
+        require(msg.sender == _owner);
+        _;
+    }
+
     function mint(address receiver, uint256 tokenId) public {
         // Check if tokenId already exists
         require(tokens[tokenId].owner == address(0), "Token ID already exists!");
+        // Check for overflow
+        require(tokenId > 0);
         tokens[tokenId].owner = receiver;
     }
 
-    function transfer(address _to, uint256 _tokenId) public {
+    function transfer(address _to, uint256 _tokenId) public onlyOwner(tokens[_tokenId].owner) {
         require(tokens[_tokenId].owner != _to);
         tokens[_tokenId].owner = _to;
         emit Transfer(msg.sender, _to, _tokenId);
@@ -26,6 +34,7 @@ contract ERC721 {
 
     function balanceOf(address _owner) public view returns (uint256) {
         uint256 balance = 0;
+        // Use a more efficient looping technique to prevent gas issues if the array of tokens grows too large
         for (uint256 i = 0; i < tokens.length; i++) {
             if (tokens[i].owner == _owner) {
                 balance++;
